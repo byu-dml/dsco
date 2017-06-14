1e1ff5920d88eac8f490cb6ced40e0c14097794e
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2017-06-13 19:13:54 -0600

    added notes, docs, and dsco db connection tool
    the host and user names are missing so that they are not made publicly available

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-dsco
+This repository contains all notes, documentation, and source code created by the BYU Data Mining Lab in its interactions with the company Dsco.
+
+Contributors:
+Brandon Schoenfeld
+Christophe Giraud-Carrier
---
+++ src/dscodbclient.py
@@ -0,0 +1,78 @@
+from sshtunnel import SSHTunnelForwarder
+from pymongo import MongoClient
+
+class DscoDBClient():
+
+    # def __init__(self, ssh_pkey='/home/user/.ssh/id_rsa'):
+    #     # manual ssh key authentication
+    #     from getpass import getpass   # move to top of page
+    #     MONGO_HOST =
+    #     MONGO_USER =
+    #     self.server = SSHTunnelForwarder(
+    #         MONGO_HOST,
+    #         ssh_username=MONGO_USER,
+    #         remote_bind_address=('127.0.0.1', 27017),
+    #         ssh_pkey=pkey,
+    #         ssh_private_key_password=getpass('ssh private key password: ')
+    #     )
+
+    def __init__(self):
+        # automatic ssh key authentication
+        MONGO_HOST =
+        MONGO_USER =
+        self.server = SSHTunnelForwarder(
+            MONGO_HOST,
+            ssh_username=MONGO_USER,
+            remote_bind_address=('127.0.0.1', 27017)
+        )
+
+    def start(self):
+        # connect to server and create mongodb client
+        if not self.server.is_active:
+            self.server.start()
+            self.client = MongoClient(self.remote_bind_ip, self.server.local_bind_port)
+        return self
+
+    def stop(self):
+        # disconnect from server
+        if self.server.is_active:
+            self.server.stop()
+            self.client = None
+        return self
+
+    def getMongoDBClient(self):
+        return self.client
+
+
+    def __del__(self):
+        if self.server.is_active == True:
+            self.stop()
+
+def execute_db_transaction(f):
+    result = None
+    dsco_client = DscoDBClient()
+    try:
+        dsco_client.start()
+        mongo_client = dsco_client.getMongoDBClient()
+        result = f(mongo_client)
+    except Exception, e:
+        print_exc()
+    except KeyboardInterrupt:
+        print "keyboard interrupt received"
+    dsco_client.stop()
+    return result
+
+if __name__ == "__main__":
+    import json
+    # def get_database_names(mongo_client):
+    #     return mongo_client.database_names()
+    
+    # print execute_db_transaction(get_database_names)
+
+    def f(mongo_client):
+        return mongo_client.DropshipCommon.Supplier.find({}, {"name": 1})
+
+    result = execute_db_transaction(f)
+    for item in result:
+        print json.dumps(item, sort_keys=True, indent=2, separators=(",", ": "))
+        break
